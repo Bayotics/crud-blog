14b213afe0a6454887188257c5cf51e95e7294b7
Abdullahi Shobaloju <sabdullahi@cinnsol.com>
2024-06-04 22:45:31 +0100

    downloading blog app to a nw folder

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import './App.css';
+import MAinNav from './components/MAinNav.jsx';
+import NavAnnoucement from './components/NavAnnouncement';
+import HomePage from './pages/HomePage';
+import SigninScreen from './pages/Signin';
+import SignupScreen from './pages/Signup';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='main'>
+        <header>
+          <NavAnnoucement />
+          <MAinNav />
+        </header>
+        <main>
+          <div className='main-content'>
+            <Routes>
+              <Route path='/' element = {<HomePage />} />
+              <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
+            </Routes>
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/NavAnnouncement.jsx
@@ -0,0 +1,11 @@
+
+
+const NavAnnoucement = () => {
+    return(
+        <div className="nav-announcement-main bg-black-100 text-blue-600 ">
+            Hello
+        </div>
+    )
+}
+
+export default NavAnnoucement
